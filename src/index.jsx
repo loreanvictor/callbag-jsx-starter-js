6c1d90ecc2cd52b32b46049717e2eb9366fa00cb
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2020-12-18 13:48:53 +0100

    update deps

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,21 @@
+import { expr, filter, interval, map } from 'callbag-common'
 import { makeRenderer } from 'callbag-jsx'
+import { state } from 'callbag-state'
 
 const renderer = makeRenderer()
-renderer.render(<div>Hellow World!</div>).on(document.body)
+const name = state('World')
+const timer = interval(1000)
+
+const format = name => name[0].toUpperCase() + name.substr(1)
+const displayName = name |> filter(n => n.length > 0) |> map(format)
+
+renderer.render(
+  <>
+    <div>Hellow {displayName}!</div>
+    <input _state={name} type='text' placeholder='name ...'/>
+
+    <br/><br/>
+
+    <div><small>You have been here for {expr($ => $(timer, -1) + 1)} seconds!</small></div>
+  </>
+).on(document.body)
